215f779536b29ea7ee94d3ce365d179c3cadc54b
AL Riyad <imalriyad@gmail.com>
2024-07-13 02:00:22 +0600

    navbar mobile menu adde" "\

--- src/header/Navbar.jsx
+++ src/header/Navbar.jsx
@@ -1,5 +1,4 @@
 import { FiSearch } from "react-icons/fi";
-import { FaHeart } from "react-icons/fa6";
 import { TiShoppingCart } from "react-icons/ti";
 import { CgProfile } from "react-icons/cg";
 import { IoMenu, IoClose } from "react-icons/io5";
@@ -9,9 +8,10 @@ import { useState } from "react";
 const Navbar = () => {
   const [isOpenMenu, setIsOpenMenu] = useState(false);
   const [isOpenSearchBox, setIsOpenSearchBox] = useState(false);
+
   return (
     <>
-      <header className="bg-blue-500 ">
+      <header className="bg-color-primary">
         <div className="max-w-screen-2xl mx-auto px-4 lg:px-14 md:px-8 py-2">
           <div className="flex items-center justify-between">
             <div
@@ -34,34 +34,31 @@ const Navbar = () => {
             >
               <input
                 type="text"
-                placeholder="Type here"
-                className={`input focus:outline-none md:block w-full rounded-full ${
-                  isOpenSearchBox ? "block" : "hidden"
+                placeholder="Search here"
+                className={`input focus:outline-none placeholder:text-gray-400 md:block w-full rounded-full ${
+                  isOpenSearchBox ? "block input-sm md:input-md" : "hidden"
                 }`}
               />
               <div
                 onClick={() => setIsOpenSearchBox(true)}
                 className={`absolute md:right-6 md:top-2  block top-[-14px] right-[10px] ${
-                  isOpenSearchBox ? "top-[10px] right-[22px] text-blue-500" : ""
+                  isOpenSearchBox ? "top-[1px] right-[22px] " : ""
                 }`}
               >
                 <FiSearch
-                  className={`text-3xl md:text-blue-500 text-white ${
-                    isOpenSearchBox ? "text-blue-500" : ""
+                  className={`text-3xl md:text-color-primary   ${
+                    isOpenSearchBox ? "text-color-primary" : "text-white"
                   }`}
                 />
               </div>
             </div>
 
             {/* Navitem */}
-            <div className="justify-end list-none flex gap-6 items-center">
-              <li className="text-2xl lg:block hidden text-white">
-                <FaHeart />
-              </li>
-              <li className="text-3xl lg:block hidden text-white">
+            <div className="justify-end list-none flex gap-10 items-center">
+              <li className="text-4xl lg:block hidden text-white">
                 <TiShoppingCart />
               </li>
-              <li className="text-3xl lg:block hidden text-white">
+              <li className="text-4xl lg:block hidden text-white">
                 {" "}
                 <CgProfile />
               </li>
@@ -84,28 +81,35 @@ const Navbar = () => {
 
       {/* Mobile menu  */}
       <div
-        className={`min-h-screen lg:hidden bg-blue-500 w-[240px] list-none ${
-          isOpenMenu ? "block" : "hidden"
+        className={`min-h-screen overflow-x-hidden lg:hidden bg-blue-800 w-full list-none transform transition-transform duration-1000 ${
+          isOpenMenu ? "translate-x-0" : "-translate-x-full"
         }`}
       >
-        <ul className="flex items-center gap-8 p-4 border-t border-b justify-center">
-          <li className="text-2xl text-white">
-            <FaHeart />
-          </li>
-          <li className="text-3xl text-white">
+        <ul className="flex items-center p-6 px-0 justify-around">
+          <li className="text-3xl btn btn-sm bg-color-primary hover:bg-blue-900  text-white">
             <TiShoppingCart />
           </li>
-          <li className="text-3xl text-white">
+          <li className="text-3xl btn btn-sm bg-color-primary hover:bg-blue-900 text-white">
             {" "}
             <CgProfile />
           </li>
         </ul>
-        <ul className="flex flex-col gap-4 list-none p-10 pt-4 ">
-          <li className="text-xl text-white border-b pb-2">Health Care</li>
-          <li className="text-xl text-white border-b pb-2">Health Care</li>
-          <li className="text-xl text-white border-b pb-2">Health Care</li>
-          <li className="text-xl text-white border-b pb-2">Health Care</li>
-          <li className="text-xl text-white border-b pb-2">Health Care</li>
+        <ul className="flex flex-col gap-2 list-none px-10 py-6 pt-0">
+        
+          <li className="text-lg hover:bg-blue-900 text-white p-2">
+            Health Care
+          </li>
+          <li className="text-lg hover:bg-blue-900 text-white p-2">
+            Orthopedic
+          </li>
+          <li className="text-lg hover:bg-blue-900 text-white p-2">Skeleton</li>
+          <li className="text-lg hover:bg-blue-900 text-white p-2">SURGICAL</li>
+          <li className="text-lg hover:bg-blue-900 text-white p-2">
+            MEDICAL EQUIPMENT
+          </li>
+          <li className="text-lg hover:bg-blue-900 text-white p-2">
+            PHYSIOTHERAPY MACHINE
+          </li>
         </ul>
       </div>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "color-primary": "#1d4ed8",
+      },
+    },
   },
   plugins: [require("daisyui")],
 };
