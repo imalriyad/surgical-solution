289aa0ac42d69f6f48c777ef723a0ce393971fae
AL Riyad <imalriyad@gmail.com>
2024-07-14 15:23:12 +0600

    hero section first slider done

--- src/header/Hero.jsx
+++ src/header/Hero.jsx
@@ -1,44 +1,125 @@
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthProvider";
-
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import heroimg from "../assets/heroimg.png";
+// import required modules
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 const Hero = () => {
-    const {isOpenMenu} = useContext(AuthContext);
+  const { isOpenMenu } = useContext(AuthContext);
 
-    return (
-        <>
-           <div className={isOpenMenu?'hidden lg:block':'block'}>
-           <div className="carousel w-full">
-  <div id="item1" className="carousel-item w-full">
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.jpg"
-      className="w-full" />
-  </div>
-  <div id="item2" className="carousel-item w-full">
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg"
-      className="w-full" />
-  </div>
-  <div id="item3" className="carousel-item w-full">
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg"
-      className="w-full" />
-  </div>
-  <div id="item4" className="carousel-item w-full">
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.jpg"
-      className="w-full" />
-  </div>
-</div>
-<div className="flex w-full justify-center gap-2 py-2">
-  <a href="#item1" className="btn btn-xs">1</a>
-  <a href="#item2" className="btn btn-xs">2</a>
-  <a href="#item3" className="btn btn-xs">3</a>
-  <a href="#item4" className="btn btn-xs">4</a>
-</div>
-           </div>
-        </>
-    );
+  return (
+    <>
+      <div className={isOpenMenu ? "hidden lg:block" : "block"}>
+        <Swiper
+          spaceBetween={30}
+          centeredSlides={true}
+          // autoplay={{
+          //   delay: 2500,
+          //   disableOnInteraction: false,
+          // }}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          modules={[Autoplay, Pagination, Navigation]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <div className="h-[83vh] bg-blue-800 w-full p-10">
+              <div className="flex items-center gap-4 max-w-screen-2xl mx-auto px-4 lg:px-14 md:px-8 py-2">
+                <div className="w-full text-white">
+                  <h1 className="text-3xl font-bold">
+                    {" "}
+                    Surgical solution{" "}
+                    <span className="text-3xl LalSabujNormal">
+                      এর যেকোন ইকুইপমেন্ট আপনার প্রয়োজনে পেয়ে যাবেন ঘরে বসেই
+                      ক্যাশ অন ডেলিভারিতে !!{" "}
+                    </span>
+                  </h1>
+                  <div className="space-y-2 pt-5 text-lg tracking-tight">
+                    <li className="LalSabujNormal">
+                      ✅ একমাত্র আমরাই সারাদেশ ব্যাপী দিচ্ছি মেডিকেল,
+                      অথোপেডিক্স, স্পোর্টস ফিজিওথেরাপি, ফিজিওথেরাপি, সায়ন্টেফিক,
+                      ডায়াগনস্টিক সহ সকল ইকুইপমেন্ট হাতে পাওয়ার সেরা মানের
+                      নিশ্চয়তা
+                    </li>
+                    <li className="LalSabujNormal">
+                      ✅ <span className="">Surgical solution</span> আপনার
+                      প্রয়োজনীয় সকল ইকুইপমেন্ট সেরা মান এবং দামের মধ্যে সরবরাহ
+                      করতে সব সময় প্রস্তুত !!
+                    </li>
+                    <li className="LalSabujNormal">
+                      ✅ আমরা-ই সরাসরি ইম্পোর্ট করে থাকি আলহামদুলিল্লাহ।
+                    </li>
+
+                    <li className="LalSabujNormal">
+                      ✅ এছাড়া আমরা গ্যারান্টি এবং ওয়ারেন্টির ব্যাপারে খুবই ভালো
+                      সার্ভিস দিয়ে যাচ্ছি।
+                    </li>
+                  </div>
+                </div>
+                <img
+                  src={heroimg}
+                  alt=""
+                  className="w-6/12 h-full rounded-lg object-contain"
+                />
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="h-[80vh]">
+              <img
+                src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg"
+                alt=""
+                className="h-full w-full"
+              />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="h-[80vh]">
+              <img
+                src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg"
+                alt=""
+                className="h-full w-full"
+              />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="h-[80vh]">
+              <img
+                src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg"
+                alt=""
+                className="h-full w-full"
+              />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="h-[80vh]">
+              <img
+                src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg"
+                alt=""
+                className="h-full w-full"
+              />
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="h-[80vh]">
+              <img
+                src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg"
+                alt=""
+                className="h-full w-full"
+              />
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </>
+  );
 };
 
 export default Hero;
